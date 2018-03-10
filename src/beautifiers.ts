458f01b9ad72efefaf89d9a9153e28e2d9e302ac
Glavin Wiechert <glavin.wiechert@gmail.com>
2018-03-09 22:36:10 -0400

    Add more beautifiers

--- src/beautifiers.ts
+++ src/beautifiers.ts
@@ -1,38 +1,12 @@
 import { Beautifier } from "unibeautify";
-import Prettier from "@unibeautify/beautifier-prettier";
-// const PrettyDiff: Beautifier = require("beautifier-prettydiff");
+import prettyDiff from "@unibeautify/beautifier-prettydiff";
+import prettier from "@unibeautify/beautifier-prettier";
+import jsBeautify from "@unibeautify/beautifier-js-beautify";
+import eslint from "@unibeautify/beautifier-eslint";
 
-// const CustomBeautifier: Beautifier = {
-//     name: "Test",
-//     options: {
-//         _: true,
-//         CSS: true,
-//         JavaScript: false,
-//         TypeScript: {
-//             // indent_size: true,
-//             new_name: "indent_size",
-//             // field1: true,
-//             indent_size(value) { // number
-//                 return value;
-//             },
-//             indent_char(value) { // string
-//                 return value;
-//             },
-//             indent_preamble(value) { // boolean
-//                 return value;
-//             },
-//             field3: [["indent_size", "indent_char"], (options) => {
-//                 return options.indent_size + options.indent_char;
-//             }]
-//         }
-//     },
-//     beautify(data) {
-//         return Promise.resolve("");
-//     }
-// };
-
-export const beautifiers: Beautifier[] = [
-  Prettier
-  // PrettyDiff,
-  // CustomBeautifier
+export const beautifiers: Beautifier[] = <any[]>[
+  prettier,
+  jsBeautify,
+  prettyDiff,
+  eslint,
 ];
--- src/index.ts
+++ src/index.ts
@@ -5,26 +5,38 @@ import unibeautify from "unibeautify";
 import { beautifiers } from "./beautifiers";
 
 export function activate(context: vscode.ExtensionContext) {
-    unibeautify.loadBeautifiers(beautifiers);
-    const { supportedLanguages } = unibeautify;
-    const languageFilters: vscode.DocumentFilter[] = supportedLanguages.reduce((filters, { vscodeLanguages }) => {
-        const languages: vscode.DocumentFilter[] = vscodeLanguages.map(language => ({ language }));
-        return [...filters, ...languages];
-    }, [] as vscode.DocumentFilter[]);
-    const patternFilters: vscode.DocumentFilter[] = supportedLanguages.map(language => ({
-        pattern: `**/*.{${language.extensions.join(',')}}`
+  unibeautify.loadBeautifiers(beautifiers);
+  const { supportedLanguages } = unibeautify;
+  const languageFilters: vscode.DocumentFilter[] = supportedLanguages.reduce(
+    (filters, { vscodeLanguages = [] }) => {
+      const languages: vscode.DocumentFilter[] = vscodeLanguages.map(
+        language => ({ language })
+      );
+      return [...filters, ...languages];
+    },
+    [] as vscode.DocumentFilter[]
+  );
+  const patternFilters: vscode.DocumentFilter[] = supportedLanguages
+    .filter(language => language.extensions.length > 0)
+    .map(language => ({
+      pattern: `**/*{${language.extensions.join(",")}}`
     }));
-    const documentSelector: vscode.DocumentSelector = [
-        ...languageFilters,
-        ...patternFilters,
-    ];
-    console.log("Unibeautify documentSelector", documentSelector);
-    const editProvider = new EditProvider();
-    context.subscriptions.push(
-        vscode.languages.registerDocumentFormattingEditProvider(documentSelector, editProvider),
-        vscode.languages.registerDocumentRangeFormattingEditProvider(documentSelector, editProvider),
-    );
-};
+  const documentSelector: vscode.DocumentSelector = [
+    ...languageFilters,
+    ...patternFilters
+  ];
+  console.log("Unibeautify documentSelector", documentSelector);
+  const editProvider = new EditProvider();
+  context.subscriptions.push(
+    vscode.languages.registerDocumentFormattingEditProvider(
+      documentSelector,
+      editProvider
+    ),
+    vscode.languages.registerDocumentRangeFormattingEditProvider(
+      documentSelector,
+      editProvider
+    )
+  );
+}
 
-export function deactivate() {
-};
+export function deactivate() {}
