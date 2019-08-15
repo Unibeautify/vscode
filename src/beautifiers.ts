f6ce3369faca6274891868cdcb0ca66182799af0
Wilinton Aguirre Ortiz <waguirre@nubixar.com>
2019-08-14 22:28:17 -0500

    chore(beautifiers): Baetify beautifiers.ts

--- src/beautifiers.ts
+++ src/beautifiers.ts
@@ -1,13 +1,13 @@
 import { Beautifier } from "unibeautify";
-import prettyDiff from "@unibeautify/beautifier-prettydiff";
-import prettier from "@unibeautify/beautifier-prettier";
-import jsBeautify from "@unibeautify/beautifier-js-beautify";
-import eslint from "@unibeautify/beautifier-eslint";
 import phpCsFixer from "@unibeautify/beautifier-php-cs-fixer";
 import clangFormat from "@unibeautify/beautifier-clang-format";
+import eslint from "@unibeautify/beautifier-eslint";
+import jsBeautify from "@unibeautify/beautifier-js-beautify";
 import phpCodesniffer from "@unibeautify/beautifier-php-codesniffer";
-import sqlFormat from "@unibeautify/beautifier-sqlformat";
 import csscomb from "@unibeautify/beautifier-csscomb";
+import prettier from "@unibeautify/beautifier-prettier";
+import prettyDiff from "@unibeautify/beautifier-prettydiff";
+import sqlFormat from "@unibeautify/beautifier-sqlformat";
 import tslint from "@unibeautify/beautifier-tslint";
 
 export const beautifiers: Beautifier[] = <any[]>[
@@ -20,5 +20,5 @@ export const beautifiers: Beautifier[] = <any[]>[
   phpCodesniffer,
   sqlFormat,
   csscomb,
-  tslint
+  tslint,
 ];
